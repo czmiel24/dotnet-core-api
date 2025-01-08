ecbebf63f923c7c7bf8d655a684ab77a134107c8
czmiel24 <58327274+czmiel24@users.noreply.github.com>
2025-01-08 17:09:21 +0000

    test

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -11,7 +11,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")] 
+    [Route("backend/workflow/api/[controller]")] 
     [ApiController]
     public class TodoController : Controller
     {
